be7f47e28c5dcbbc358aecdee9aea87d594e85d1
swati1752 <swatibansal1752@gmail.com>
2021-12-20 22:18:40 +0530

    client side js file added

--- mod/forecast.js
+++ mod/forecast.js
@@ -1,7 +1,7 @@
 const request = require("postman-request");
 // const geocoding = require('geocoding');
 const forecast = (x , y , callback) =>{
-    const url2 = `http://api.weatherstack.com/current?access_key=d4ccf933ce01e0e713f676ed3d7232e1&query=${x},${y}&units=f`;
+    const url2 = `http://api.weatherstack.com/current?access_key=d4ccf933ce01e0e713f676ed3d7232e1&query=${x},${y}`;
     request({url:url2 , JSON:true},(error, res)=>{
         if(error){
             callback("Unable to connect to location services",undefined);
@@ -11,7 +11,7 @@ const forecast = (x , y , callback) =>{
         }
         else {
             const d = JSON.parse(res.body);
-            const forecast = `Temperature of the place at time ${d.current.observation_time} is ${d.current.temperature} and visiblity is ${d.current.visibility}`;
+            const forecast = `Temperature of the place at time ${d.current.observation_time} is ${d.current.temperature} and visibility is ${d.current.visibility}`;
             callback(error,forecast);
         }
         }      
--- public/js/app.js
+++ public/js/app.js
@@ -1,6 +1,3 @@
-
-console.log("CLient");
-
 const weatherform = document.querySelector('form');
 const search = document.querySelector('input');
 const messageOne = document.querySelector('#message-1');
@@ -9,23 +6,24 @@ const messageTwo = document.querySelector('#message-2');
 
 
 
-weatherform.addEventListener('sumbit' ,(e) => {
+weatherform.addEventListener('submit' ,(e) => {
     e.preventDefault()
-    console.log("testing");
-    // const location = search.value
-    // document.querySelector('#message-1').innerHTML = location;
-    // messageOne.textContent = "Loading..."; 
-    //  fetch(`http://localhost:3000/weather?address=!`).then((res) => {
-    //     res.json().then((data) =>{
-    //         if(data.err) {
-    //             console.log(data.error)
-    //         }
-    //         else{
-    //             messageOne.textContent = data.location
-    //             messageTwo.textContent = data.forecast
-    //         }
-    //     })
-    // })
+    // console.log("testing");
+    const location = search.value
+    document.querySelector('#message-1').innerHTML = location;
+    messageOne.textContent = "Loading..."; 
+    messageTwo.textContent = "";
+     fetch(`http://localhost:3000/weather?address=${location}`).then((res) => {
+        res.json().then((data) =>{
+            if(data.err) {
+                console.log(data.error)
+            }
+            else{
+                messageOne.textContent = data.location;
+                messageTwo.textContent = data.forecast;
+            }
+        })
+    })
 });
 
 
