292c9b388877a81d45ee1ed7cf6787c1ada0f407
swati1752 <swatibansal1752@gmail.com>
2021-12-21 06:13:55 +0530

    unnecessary comment removed

--- mod/forecast.js
+++ mod/forecast.js
@@ -1,5 +1,4 @@
 const request = require("postman-request");
-// const geocoding = require('geocoding');
 const forecast = (x , y , callback) =>{
     const url2 = `http://api.weatherstack.com/current?access_key=d4ccf933ce01e0e713f676ed3d7232e1&query=${x},${y}`;
     request({url:url2 , JSON:true},(error, res)=>{
@@ -11,7 +10,7 @@ const forecast = (x , y , callback) =>{
         }
         else {
             const d = JSON.parse(res.body);
-            const forecast = `Temperature of the place at time ${d.current.observation_time} is ${d.current.temperature} and visibility is ${d.current.visibility}`;
+            const forecast = `Temperature of the place at time ${d.current.observation_time} is ${d.current.temperature}°C and visibility is ${d.current.visibility}`;
             callback(error,forecast);
         }
         }      
